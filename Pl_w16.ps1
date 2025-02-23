9cc444e08cbb7f88532a933dccf3fa4950268070
und3cr1pted <138370391+und3cr1pted@users.noreply.github.com>
2025-02-23 13:23:08 +0100

    Delete Pl_w16.ps1

--- Pl_w16.ps1
+++
@@ -1,8 +0,0 @@
-[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-$template = [Windows.UI.Notifications.ToastTemplateType]::ToastText01
-$xml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($template)
-$xmlString = $xml.GetXml()
-$xmlString = $xmlString.Replace('<text id="1">', '<text id="1">Wave 16 incoming (Wargames: Torn Apart)')
-$xml.LoadXml($xmlString)
-$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Plankerton Endurance').Show($toast)
